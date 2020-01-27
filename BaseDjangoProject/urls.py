d6e36b2051416abc2cf2373e6b2d7e4f4255ae40
Hashibei <josepholiveiracorrea@uninove.br>
2020-01-27 14:39:45 -0300

    Added url new
    Url account/new will be used to create a new user account.

--- BaseDjangoProject/urls.py
+++ BaseDjangoProject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls', 'account'))
+    path('account/', include(("account.urls", "account")))
 ]
